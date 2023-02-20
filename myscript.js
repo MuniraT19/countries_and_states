4adbb6e7f1997052ede4f3be8b5268b9f19e3b25
Munira Tabassum <munira.mt2013@gmail.com>
2023-02-20 04:43:38 -0500

    first dropdown added

--- myscript.js
+++ myscript.js
@@ -1,21 +1,24 @@
-// console.log("JS is working");
-let countries = null;
-fetch("https://xc-countries-api.fly.dev/api/countries/")
-    .then(response => response.json())
-    .then(data => {
-        countries = data;
-        console.log("countries", countries);
-    });
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.responseText);
+}
+
+let countries = httpGet("https://xc-countries-api.fly.dev/api/countries/");
+console.log(countries);
+
+
 window.onload = function(){
-    // const countries = ["United States", "Canada", "Mexico", "Brazil", "Argentina", "Peru", "Chile", "Colombia", "Venezuela", "Uruguay"];
-          const countrySelect = document.getElementById("countrySelect");
-    
-          // Loop through the countries array and add each country to the dropdown
-          for (let i = 0; i < countries.length; i++) {
-            const option = document.createElement("option");
-            option.text = countries[i].name;
-            option.value = countries[i].code;
-            countrySelect?.add(option);
-            console.log(countrySelect)
-          }
- };
+    let countrySelect = document.getElementById("countrySelect");
+    for (i=0; i<countries.length; i++){ 
+        console.log(countries[i]);
+        let opt = document.createElement("option");
+        opt.value=countries[i].code;
+        opt.text=countries[i].name;
+        countrySelect.add(opt);
+
+    }
+
+    console.log(countrySelect);
+};
